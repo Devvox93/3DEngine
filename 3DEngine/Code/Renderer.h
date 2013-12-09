ec4a27be35835c80079ee38646c17ad0efcbeaaf
hammersyko <alinor.hammersyko@gmail.com>
2013-12-09 12:42:30 +0100

    First start at Renderer

--- 3DEngine/Code/Renderer.h
+++ 3DEngine/Code/Renderer.h
@@ -1,8 +1,16 @@
-#pragma once
+#include <Windows.h>
+#include <mmsystem.h>
+#include <d3dx9.h>
+#include <strsafe.h>
+
 class Renderer
 {
 public:
 	Renderer();
 	~Renderer();
+	HRESULT InitD3D(HWND hWnd);
+	VOID Cleanup();
+	VOID SetupMatrices();
+	LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 };
 
