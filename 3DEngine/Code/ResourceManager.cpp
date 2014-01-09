1cd17acee9882a91379423812cbf78709513eb31
Nick <n.dachard@outlook.com>
2014-01-09 13:06:58 +0100

    lol

--- 3DEngine/Code/ResourceManager.cpp
+++ 3DEngine/Code/ResourceManager.cpp
@@ -3,13 +3,50 @@
 #include "Logger.h"
 #include <map>
 #include "windows.h"
+#include <iostream>
 #include <sstream>
+#include <string.h>
+#include <string>
   
 std::map<char*, Resource> ResourceMap;
 
 
 void ResourceManager::storeResource(char *path)
 {
+	
+
+	std::stringstream sstm;
+	sstm << "Path = " << path;
+	Logger::getInstance().log(INFO, sstm.str());
+
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "BMP") {
+		// aanmaken BMP-ResourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "x") {
+		// aanmaken X-resourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "m2") {
+		// aanmaken X-resourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "png") {
+		// aanmaken X-resourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "m") {
+		// aanmaken X-resourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	if (sstm.str().substr(sstm.str().find_last_of(".") + 1) == "jpg") {
+		// aanmaken X-resourceLoader
+		Logger::getInstance().log(INFO, sstm.str());
+	}
+	Logger::getInstance().log(INFO, sstm.str());
+	
+	
+	
 	if (ResourceMap.find(path) == ResourceMap.end()){
 		Resource *hmprsrc = new Resource(path);
 		ResourceMap.insert(std::make_pair(path, *hmprsrc));
