3ced5650fb6c39f1cefda2dc29c22c8b43fa1fe4
ManIkWeet <spamme2010@live.nl>
2014-01-22 12:52:20 +0100

    Geen crash meer als X niet goed geladen is. Jordi heeft een aantal mooie modellen gekregen. Draaipunt van camera is nu 0,n,0

--- 3DEngine/Code/Defines.h
+++ 3DEngine/Code/Defines.h
@@ -13,8 +13,8 @@ Purpose: Loads X resources.
 
 #define DIRECTINPUT_VERSION 0x0800
 #define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ | D3DFVF_TEX1)
-#define M_PI 3.14159265358979323846
-#define RADIANS(degree) (M_PI * (degree) / 180)
+#define M_PI 3.14159265358979323846f
+#define RADIANS(degree) (M_PI * (degree) / 180.0f)
 #define DEGREES(radian) (radian) * (180 / M_PI)
 #define SPEED 0.5f
 
