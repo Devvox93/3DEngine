9128a72742307ea0170a15aa19c3cabf2d183804
hammersyko <alinor.hammersyko@gmail.com>
2014-01-23 14:40:45 +0100

    namen mogen fout zijn, dan worden deze niet gemaakt/gerenderd

--- 3DEngine/Code/Scene.cpp
+++ 3DEngine/Code/Scene.cpp
@@ -74,7 +74,11 @@ void Scene::readSceneFile(std::vector<std::string>* sceneFile, ResourceManager *
 
 		if (lineType.compare("skybox") == 0)
 		{
-			skybox = new Skybox(lineInfo, resourceManager);
+			std::ifstream file(lineInfo);
+			if (file)
+			{
+				skybox = new Skybox(lineInfo, resourceManager);
+			}
 		}
 		else if (lineType.compare("heightmap") == 0)
 		{
@@ -82,42 +86,54 @@ void Scene::readSceneFile(std::vector<std::string>* sceneFile, ResourceManager *
 
 			std::string heightmap = lineInfo.substr(0, comma);
 			std::string terrainTexture = lineInfo.substr(comma + 1, lineInfo.size()-colon-1);
-			terrain = new Terrain(heightmap, terrainTexture, resourceManager);
+
+			std::ifstream fileHeightmap(heightmap);
+			std::ifstream fileTerrainTexture(terrainTexture);
+			if (fileHeightmap && fileTerrainTexture)
+			{
+				terrain = new Terrain(heightmap, terrainTexture, resourceManager);
+			}
 		}
 		else if (lineType.compare("entity") == 0)
 		{
 			size_t nameComma = lineInfo.find_first_of(",");
-			size_t positionXComma = lineInfo.find(",", nameComma + 1);
-			size_t positionYComma = lineInfo.find(",", positionXComma + 1);
-			size_t positionZComma = lineInfo.find(",", positionYComma + 1);
-			size_t yawComma = lineInfo.find(",", positionZComma + 1);
-			size_t pitchComma = lineInfo.find(",", yawComma + 1);
-			size_t rollComma = lineInfo.find(",", pitchComma + 1);
-			size_t scaleXComma = lineInfo.find(",", rollComma + 1);
-			size_t scaleYComma = lineInfo.find(",", scaleXComma + 1);
-
 			std::string xResourceName = lineInfo.substr(0, nameComma);
-			std::string positionX = lineInfo.substr(nameComma + 1, positionXComma - nameComma - 1);
-			std::string positionY = lineInfo.substr(positionXComma + 1, positionYComma - positionXComma - 1);
-			std::string positionZ = lineInfo.substr(positionYComma + 1, positionZComma - positionYComma - 1);
-			std::string yaw = lineInfo.substr(positionZComma + 1, yawComma - positionZComma - 1);
-			std::string pitch = lineInfo.substr(yawComma + 1, pitchComma - yawComma - 1);
-			std::string roll = lineInfo.substr(pitchComma + 1, rollComma - pitchComma - 1);
-			std::string scaleX = lineInfo.substr(rollComma + 1, scaleXComma - rollComma - 1);
-			std::string scaleY = lineInfo.substr(scaleXComma + 1, scaleYComma - scaleXComma - 1);
-			std::string scaleZ = lineInfo.substr(scaleYComma + 1, infoString.size() - scaleYComma - 1);
 
-			float positionXFloat = (float)atof(positionX.c_str());
-			float positionYFloat = (float)atof(positionY.c_str());
-			float positionZFloat = (float)atof(positionZ.c_str());
-			float yawFloat = (float)atof(yaw.c_str());
-			float pitchFloat = (float)atof(pitch.c_str());
-			float rollFloat = (float)atof(roll.c_str());
-			float scaleXFloat = (float)atof(scaleX.c_str());
-			float pscaleYFloat = (float)atof(scaleY.c_str());
-			float scaleZFloat = (float)atof(scaleZ.c_str());
+			std::ifstream fileResource(xResourceName);
+			if (fileResource)
+			{
+				size_t positionXComma = lineInfo.find(",", nameComma + 1);
+				size_t positionYComma = lineInfo.find(",", positionXComma + 1);
+				size_t positionZComma = lineInfo.find(",", positionYComma + 1);
+				size_t yawComma = lineInfo.find(",", positionZComma + 1);
+				size_t pitchComma = lineInfo.find(",", yawComma + 1);
+				size_t rollComma = lineInfo.find(",", pitchComma + 1);
+				size_t scaleXComma = lineInfo.find(",", rollComma + 1);
+				size_t scaleYComma = lineInfo.find(",", scaleXComma + 1);
 
-			models.push_back(new Model((XResource*)resourceManager->getResource(xResourceName), positionXFloat, positionYFloat, positionZFloat, yawFloat, pitchFloat, rollFloat, scaleXFloat, pscaleYFloat, scaleZFloat));
+				std::string positionX = lineInfo.substr(nameComma + 1, positionXComma - nameComma - 1);
+				std::string positionY = lineInfo.substr(positionXComma + 1, positionYComma - positionXComma - 1);
+				std::string positionZ = lineInfo.substr(positionYComma + 1, positionZComma - positionYComma - 1);
+				std::string yaw = lineInfo.substr(positionZComma + 1, yawComma - positionZComma - 1);
+				std::string pitch = lineInfo.substr(yawComma + 1, pitchComma - yawComma - 1);
+				std::string roll = lineInfo.substr(pitchComma + 1, rollComma - pitchComma - 1);
+				std::string scaleX = lineInfo.substr(rollComma + 1, scaleXComma - rollComma - 1);
+				std::string scaleY = lineInfo.substr(scaleXComma + 1, scaleYComma - scaleXComma - 1);
+				std::string scaleZ = lineInfo.substr(scaleYComma + 1, infoString.size() - scaleYComma - 1);
+
+				float positionXFloat = (float)atof(positionX.c_str());
+				float positionYFloat = (float)atof(positionY.c_str());
+				float positionZFloat = (float)atof(positionZ.c_str());
+				float yawFloat = (float)atof(yaw.c_str());
+				float pitchFloat = (float)atof(pitch.c_str());
+				float rollFloat = (float)atof(roll.c_str());
+				float scaleXFloat = (float)atof(scaleX.c_str());
+				float pscaleYFloat = (float)atof(scaleY.c_str());
+				float scaleZFloat = (float)atof(scaleZ.c_str());
+
+			
+				models.push_back(new Model((XResource*)resourceManager->getResource(xResourceName), positionXFloat, positionYFloat, positionZFloat, yawFloat, pitchFloat, rollFloat, scaleXFloat, pscaleYFloat, scaleZFloat));
+			}
 		}
 	}
 };
