04b50fc0479f72376eea71e061cb6fd676a67c5c
ManIkWeet <spamme2010@live.nl>
2014-01-24 13:02:08 +0100

    DirectXRenderer gebruikt nu niet meer tijdelijk een heap variabele bij het laden van een skybox/terrain.

--- 3DEngine/Code/DirectXRenderer.h
+++ 3DEngine/Code/DirectXRenderer.h
@@ -37,20 +37,11 @@ private:
 	HRESULT InitD3D(HWND hWnd, int width, int height);
 	HRESULT InitGeometry(std::string filename);
 	D3DPRESENT_PARAMETERS setMyRenderSize(int width, int height, bool activate);
-	
-	LPDIRECT3DVERTEXBUFFER9 g_pTerrainVertexBuffer = NULL; // Buffer to hold vertices
-	LPDIRECT3DINDEXBUFFER9 g_pTerrainIndexBuffer = NULL;
-	LPDIRECT3DTEXTURE9 terrainTexture = NULL;
 
-	std::map <Terrain*, LPDIRECT3DVERTEXBUFFER9*> terrainVertexBuffers;
-	std::map <Terrain*, LPDIRECT3DINDEXBUFFER9*> terrainIndexBuffers;
+	std::map <Terrain*, LPDIRECT3DVERTEXBUFFER9> terrainVertexBuffers;
+	std::map <Terrain*, LPDIRECT3DINDEXBUFFER9> terrainIndexBuffers;
 
-	LPDIRECT3DVERTEXBUFFER9 g_pSkyboxVertexBuffer = NULL; // Buffer to hold vertices
-	LPDIRECT3DINDEXBUFFER9 g_pSkyboxIndexBuffer = NULL;
-	LPDIRECT3DTEXTURE9 skyboxTexture = NULL;
-
-	std::map <Skybox*, LPDIRECT3DTEXTURE9*> skyboxTextures;
-	std::map <Skybox*, LPDIRECT3DVERTEXBUFFER9*> skyboxVertexBuffers;
-	std::map <Skybox*, LPDIRECT3DINDEXBUFFER9*> skyboxIndexBuffers;
+	std::map <Skybox*, LPDIRECT3DVERTEXBUFFER9> skyboxVertexBuffers;
+	std::map <Skybox*, LPDIRECT3DINDEXBUFFER9> skyboxIndexBuffers;
 };
 #endif
